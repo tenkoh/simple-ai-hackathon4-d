ea43f35ea36a1dbba7775ed72b7cf5d31bcb6249
kasahara <foxguitar1202@gmail.com>
2024-09-08 15:30:34 +0900

    現文ポジ文の左右並べ

--- app.py
+++ app.py
@@ -10,6 +10,28 @@ load_dotenv()
 
 client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
+def get_article_title(url):
+    # URLからコンテンツを取得
+    response = requests.get(url)
+    
+    # レスポンスが成功したか確認
+    if response.status_code == 200:
+        # HTMLコンテンツを解析
+        soup = BeautifulSoup(response.text, 'html.parser')
+        
+        # 'article_title'クラスを持つdiv要素を検索
+        article_title = soup.find('h1', class_=["sc-uzx6gd-1", "lljVgU"])
+        
+        if article_title:
+            # div要素の内容を返す
+            return article_title.get_text(strip=True)
+        else:
+            return "Article title not found."
+    else:
+        return f"Failed to retrieve the webpage. Status code: {response.status_code}"
+
+
+
 def get_article_body(url):
     # URLからコンテンツを取得
     response = requests.get(url)
@@ -35,15 +57,21 @@ url = st.text_input("URLを入力してください")
 
 # ボタン
 if st.button("ポジティブ化する"):
-    st.write(url, "をポジティブ化します。")
-
+    title = get_article_title(url)
+    st.write(title, "をポジティブ化します。")
+    
     # URL
     model_name = "gpt-4o-mini"
+    # get_article_title(url)
 
+    original = get_article_body(url)
+
+
+    
     role="あなたはプロの心理カウンセラーです。\n"
     prompt = f'''記事の内容をポジティブにしてください。過激な表現やグロテスクな表現は避けてください。Let's think step by step.
 ## 記事
-{get_article_body(url)}
+{original}
 '''
     response = client.chat.completions.create(
         model=model_name,
@@ -52,7 +80,20 @@ if st.button("ポジティブ化する"):
             {"role": "user", "content": prompt},
         ],
     )
+    result = response.choices[0].message.content.strip()
+    # st.title("ポジティブ")
+    # st.write(response.choices[0].message.content.strip())
 
-    st.title("ポジティブ")
-    st.write(response.choices[0].message.content.strip())
 
+    # 2つの列を作成
+    col1, col2 = st.columns(2)
+
+    # 左側の列に表示する内容
+    with col1:
+        st.header("原文")
+        st.write(original)
+
+    # 右側の列に表示する内容
+    with col2:
+        st.header("ポジティブ")
+        st.write(result)
