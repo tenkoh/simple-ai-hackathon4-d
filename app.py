28b30add4202a9d8203f38fa9a755370989b677a
ﾐｸ ﾄﾓﾅﾘ <tomomiku0112@gmail.com>
2024-09-08 14:02:19 +0900

    原文とGPTだけで比較したかったが、無理そう

--- app.py
+++ app.py
@@ -8,7 +8,6 @@ load_dotenv()
 
 client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
-
 # テキスト入力
 url = st.text_input("URLを入力してください")
 
@@ -19,13 +18,27 @@ if st.button("ポジティブ化する"):
     # URL
     model_name = "gpt-4"
 
-    prompt = url + "の内容をポジティブにしてください"
+    role="あなたはプロの心理カウンセラーです。\n"
+    prompt = url + "の内容をポジティブにしてください。過激な表現やグロテスクな表現は避けてください。Let's think step by step."
     response = client.chat.completions.create(
         model=model_name,
         messages=[
-            {"role": "user", "content": prompt},
+            {"role": "user", "content": role + prompt},
         ],
     )
 
-    st.write("ポジティブ")
-    print(response.choices[0].message.content.strip())
+    #げんぶん
+    prompt_g = "以下のURLの内容をそのまま出力してください\n" + url
+    response_g = client.chat.completions.create(
+        model=model_name,
+        messages=[
+            {"role": "user", "content": prompt_g},
+        ],
+    )
+
+    st.title("原文")
+    st.write(response_g.choices[0].message.content.strip())
+
+    st.title("ポジティブニュース")
+    st.write(response.choices[0].message.content.strip())
+    
